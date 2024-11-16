58053331264ea272a763caaead53f396af440a75
mohiminulsemon <mohiminulsemon80@gmail.com>
2024-11-16 11:13:58 +0600

    app.ts code modified

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express, { Application, NextFunction, Request, Response } from "express";
 import cors from "cors";
+import { StatusCodes } from "http-status-codes";
+import globalErrorHandler from "./middlewares/globalErrorHandler";
+import router from "./app/routes";
 
 const app: Application = express();
 app.use(cors());
@@ -11,6 +14,21 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req: Request, res: Response) => {
     res.send({
         Message: "Welcome to Library Management System API..."
+    })
+});
+
+app.use('/api', router);
+
+app.use(globalErrorHandler);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.status(StatusCodes.NOT_FOUND).json({
+        success: false,
+        message: "API Route Not found",
+        error: {
+            path: req.originalUrl,
+            message: "Your API request path is not correct"
+        }
     })
 });
 
